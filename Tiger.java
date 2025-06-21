6d6df7e87964326be7597bce1c654c7d1ab3cc5f
Soham Panchal <work.panchalsoham@gmail.com>
2025-06-21 18:50:04 +0530

    Update Tiger.java

--- Tiger.java
+++ Tiger.java
@@ -1,43 +1,56 @@
-public class Tiger extends Animal implements Walk, Eat {
-    private int numberOfStripes;
-    private int speed;
-    private int soundLevel;
+import java.io.Serializable;
+
+public class Tiger extends Animal implements Walk, Serializable {
+
+    int numberOfStripes;
+    int speed;
+    int soundLevel;
 
     public Tiger() {
         super("Tiger");
     }
 
-    public void setNumberOfStripes(int numberOfStripes) {
-        this.numberOfStripes = numberOfStripes;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public void setSoundLevel(int soundLevel) {
-        this.soundLevel = soundLevel;
+    @Override
+    public String toString() {
+        return "Tiger{" +
+                "numberOfStripes=" + numberOfStripes +
+                ", speed=" + speed +
+                ", soundLevel=" + soundLevel +
+                '}';
     }
 
     public int getNumberOfStripes() {
         return numberOfStripes;
     }
 
+    public void setNumberOfStripes(int numberOfStripes) {
+        this.numberOfStripes = numberOfStripes;
+    }
+
     public int getSpeed() {
         return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 
     public int getSoundLevel() {
         return soundLevel;
     }
 
-    @Override
-    public void walking() {
-        System.out.println("I am walking at the speed " + speed);
+    public void setSoundLevel(int soundLevel) {
+        this.soundLevel = soundLevel;
     }
 
     @Override
     public void eatingCompleted() {
-        System.out.println("Tiger has finished eating.");
+        System.out.println("Tiger: I have eaten meat.");
     }
+
+    @Override
+    public void walking() {
+        System.out.println("Tiger: I am moving at the speed " + speed);
+    }
+
 }
